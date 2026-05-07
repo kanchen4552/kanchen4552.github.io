1b2e4ef4fd592d42a32bef300385f88982e656b6
kanchen4552 <40701249+kanchen4552@users.noreply.github.com>
2026-05-07 16:39:05 +0000

    Deploying to gh-pages from @ kanchen4552/kanchen4552.github.io@05c3c63f69c6cc83b7b31cec96839f4fd31c02fd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -507,7 +507,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6B%6E%63%37%30%30%35@%6D%65%64.%63%6F%72%6E%65%6C%6C.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -528,14 +528,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=1XcAQfEAAAAJ&hl=en", "_blank");
         },
       },{
       id: 'light-theme',
