e73930b5e518fa489a8d9b11c6169cb50e86847b
kanchen4552 <40701249+kanchen4552@users.noreply.github.com>
2026-05-07 06:11:00 +0000

    Deploying to gh-pages from @ kanchen4552/kanchen4552.github.io@5e867667e46acacc0a3782a61355a4a640719307 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,7 +440,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-received-2025-statistics-in-biosciences-best-paper-award",
+            },},{id: "news-paper-published-in-biometrics-sensitivity-analysis-for-attributable-effects-in-case-2-studies",
+          title: 'Paper published in Biometrics: Sensitivity Analysis for Attributable Effects in Case-2 Studies. ```...',
+          description: "",
+          section: "News",},{id: "news-received-2025-statistics-in-biosciences-best-paper-award",
           title: 'Received 2025 Statistics in Biosciences Best Paper Award',
           description: "",
           section: "News",handler: () => {
