227a0fc3ccec80534d8c12f183cfe52ca21c1719
kanchen4552 <40701249+kanchen4552@users.noreply.github.com>
2026-05-07 06:18:50 +0000

    Deploying to gh-pages from @ kanchen4552/kanchen4552.github.io@45a91b2593d70e6b01da0b350d577da629e3daee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,7 +440,10 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-paper-published-in-journal-of-the-american-statistical-association-jasa-combining-broad-and-narrow-case-definitions-in-matched-case-control-studies-firearms-in-the-home-and-suicide-risk",
+            },},{id: "news-presented-at-jsm-2024-in-the-statistics-and-health-policy-section-title-a-differential-effect-approach-to-partial-identification-of-treatment-effects",
+          title: 'Presented at JSM 2024 in the Statistics and Health Policy Section. Title: “A...',
+          description: "",
+          section: "News",},{id: "news-paper-published-in-journal-of-the-american-statistical-association-jasa-combining-broad-and-narrow-case-definitions-in-matched-case-control-studies-firearms-in-the-home-and-suicide-risk",
           title: 'Paper published in Journal of the American Statistical Association (JASA): Combining Broad and...',
           description: "",
           section: "News",},{id: "news-paper-published-in-nature-medicine-evaluating-generalizability-of-oncology-trial-results-to-real-world-patients-using-machine-learning-based-trial-emulations",
