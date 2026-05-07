54af05fac2870c45ac9518c98072cc0e26a81655
kanchen4552 <40701249+kanchen4552@users.noreply.github.com>
2026-05-07 19:33:21 +0000

    Deploying to gh-pages from @ kanchen4552/kanchen4552.github.io@350bfab74ce6de2a6dececc95a20701a8571356a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,6 +37,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "Research overview",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
